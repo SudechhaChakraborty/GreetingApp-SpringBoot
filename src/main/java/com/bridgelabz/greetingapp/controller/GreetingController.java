56ab83b71dda93596cd86ec0b4f86b8b9767a1a9
Sudechha <sudechhachakraborty@gmail.com>
2026-03-12 11:35:21 +0530

    [Sudechha] : [Added] : feat(uc2-service-layer): Implement greeting service layer

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,13 +1,20 @@
 package com.bridgelabz.greetingapp.controller;
 
+import com.bridgelabz.greetingapp.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     @GetMapping("/greeting")
     public String greeting() {
-        return "Hello World";
+        return greetingService.getGreeting();
     }
 }
